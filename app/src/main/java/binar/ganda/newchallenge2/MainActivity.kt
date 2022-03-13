918924559b88e0626129213ba1ff77364f260925
zeph1re <arlinnog1122@gmail.com>
2022-03-13 19:49:52 +0700

    Initial commit

---
+++ app/src/main/java/binar/ganda/newchallenge2/MainActivity.kt
@@ -0,0 +1,19 @@
+package binar.ganda.newchallenge2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.shopee__layout)
+    }
+
+    //Untuk menampilkan tampilan shopee
+    /*
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.shopee__layout)
+    }
+     */
+}
